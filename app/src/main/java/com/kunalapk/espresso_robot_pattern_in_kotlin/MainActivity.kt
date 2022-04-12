6ee8ed7834167eb69454dad81c0a0ea965d79a99
Kunal Pasricha <kunal.pasricha@scaler.com>
2022-04-12 12:15:02 +0530

    Foundation commit

---
+++ app/src/main/java/com/kunalapk/espresso_robot_pattern_in_kotlin/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kunalapk.espresso_robot_pattern_in_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
